384bbda4727c130e2d70cc4ef4c28886ca8179ab
eli <eliadadearaujolinhares@gmail.com>
2025-10-16 19:35:28 -0300

    Delete atividade_41.py

--- atividade_41.py
+++
@@ -1,9 +0,0 @@
-# Validação de Login com While
-# Permita que o usuário tente fazer login até digitar a senha correta ("1234"). Mostre uma mensagem de erro a cada tentativa incorreta.
-
-senha = str(input("Digite a senha: "))
-while senha == "1234":
-    print("Certo")
-    break
-else:
-    print("Erro")
